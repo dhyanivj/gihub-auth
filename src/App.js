482221ef98b0e0596daf2522172901b9e7b178cc
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2023-01-02 16:30:18 +0530

    routers added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
